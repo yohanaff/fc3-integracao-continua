aaf127cc4ac8f4d818f4227ea2351e68209933b2
Yohana Fernandes <yohana@yotec.eti.br>
2024-12-26 18:37:03 -0300

    feat: create a basic calculator

---
+++ __tests__/app.test.js
@@ -0,0 +1,17 @@
+const { add, substract, multiply, divide } = require('../src/app');
+
+test('add', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+test('substract', () => {
+    expect(substract(2, 1)).toBe(1);
+});
+
+test('multiply', () => {
+    expect(multiply(2, 3)).toBe(6);
+});
+
+test('divide', () => {
+    expect(divide(6, 3)).toBe(2);
+});
